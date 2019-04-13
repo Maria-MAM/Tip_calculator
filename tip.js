4d5541862035be071b035af173f02d9280d92fbf
Mary <mariamihaila1409@gmail.com>
2019-04-13 15:35:40 +0300

    new functionality

--- tip.js
+++ tip.js
@@ -1,37 +1,56 @@
-let input=document.querySelector('input');
+let input = document.querySelector('input');
 let valCalitate = document.querySelector('#dropQ');
+let valBacsis = document.getElementById('val-bacsis');
+let valTotala = document.getElementById('val-totala');
+let eroare = document.getElementById('eroare');
 
+// function onlyNumber(event){
+//   if ((event.keyCode<96 || event.keyCode>105) && 
+//       (event.keyCode<48 || event.keyCode>57) && 
+//       (event.keyCode!==110) && (event.keyCode!==190) && 
+//      (event.keyCode!==8) && (event.keyCode!==9)) {
+//     eroare.innerHTML="Trebuie sa introduceti doar cifre!"
+//     eroare.style.color='red';
+//     input.style.borderColor='red';
+//   }
+//   else {eroare.innerHTML="";
+//         input.style.borderColor='grey';}
+// }
 
-function onlyNo(event){
-  if ((event.keyCode<96 || event.keyCode>105) && 
-      (event.keyCode<48 || event.keyCode>57) && 
-      (event.keyCode!==110) && (event.keyCode!==190) && 
-     (event.keyCode!==8)){
-    document.getElementById('eroare').innerHTML="Trebuie sa introduceti doar cifre!"
-    document.getElementById('eroare').style.color='red';
-    document.querySelector('input').style.borderColor='red';
+function onlyNumber() {
+  if (isNaN(input.value)) {
+    eroare.innerHTML = "Trebuie sa introduceti doar cifre!"
+    eroare.style.color = 'red';
+    input.style.borderColor = 'red';
   }
-  else {document.getElementById('eroare').innerHTML="";
-        document.querySelector('input').style.borderColor='grey';}
-}
-
-input.addEventListener('keydown', onlyNo);
-
-
-
-function calcul(event){
-  if (dropQ.options[dropQ.selectedIndex].value == "bun" ) {
-    document.getElementById('val-bacsis').innerHTML= Math.round((input.value / 10)*100)/100;
-    document.getElementById('val-totala').innerHTML = Math.round((parseFloat(input.value / 10) + parseFloat(input.value))*100)/100;
-  } else 
-      if (dropQ.options[dropQ.selectedIndex].value == "mediu" ) {
-         document.getElementById('val-bacsis').innerHTML= Math.round((input.value / 20)*100)/100;
-         document.getElementById('val-totala').innerHTML = Math.round((parseFloat(input.value / 20) + parseFloat(input.value))*100)/100;
-  } else {
-    document.getElementById('val-bacsis').innerHTML= "0.00";
-    document.getElementById('val-totala').innerHTML = Math.round((input.value)*100)/100;
+  else {
+  eroare.innerHTML = "";
+    input.style.borderColor = 'grey';
   }
 }
 
+input.addEventListener('keyup', onlyNumber);
 
-dropQ.addEventListener('click', calcul);
+function calcul(event) {
+  let valCalitateServiciu = valCalitate.options[valCalitate.selectedIndex].value;
+
+  if (valCalitateServiciu == "bun" && !isNaN(input.value)) {
+    valBacsis.innerHTML = Math.round((input.value / 10) * 100) / 100;
+    valTotala.innerHTML = Math.round((parseFloat(input.value / 10) + parseFloat(input.value)) * 100) / 100;
+  } else
+    if (valCalitateServiciu == "mediu" && !isNaN(input.value)) {
+      valBacsis.innerHTML = Math.round((input.value / 20) * 100) / 100;
+      valTotala.innerHTML = Math.round((parseFloat(input.value / 20) + parseFloat(input.value)) * 100) / 100;
+    } else
+      if (((valCalitateServiciu == "prost") || (valCalitateServiciu == "default")) && !isNaN(input.value)) {
+        valBacsis.innerHTML = "0.00";
+        valTotala.innerHTML = Math.round((input.value) * 100) / 100;
+      } else {
+        valBacsis.innerHTML = "Valoare introdusa gresita!";
+        valBacsis.style.color = '#FFB5B5';
+        valTotala.innerHTML = "Valoare introdusa gresita!";
+        valTotala.style.color = '#FFB5B5';
+      }
+}
+
+valCalitate.addEventListener('input', calcul);
